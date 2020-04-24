daaaa427d494a225b47d1d6ec1d35f995217f569
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-04-24 23:03:59 +0530

    ADDED : My first servlet class

--- src/main/java/com/FirstServlet/FirstServlet.java
+++ src/main/java/com/FirstServlet/FirstServlet.java
@@ -1,7 +1,20 @@
 package com.FirstServlet;
 
-public class FirstServlet {
-    public static void main(String[] args) {
-        System.out.println("Welcome to servlet introduction");
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/FirstServlet")
+public class FirstServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>Hello World Prasad My First Servlet</h1>");
+        out.close();
     }
 }
