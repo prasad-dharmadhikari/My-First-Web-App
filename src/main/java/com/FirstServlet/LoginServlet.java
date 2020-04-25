28744392bdc6d30f5e7f81c32334f8335569dfa6
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-04-25 22:45:50 +0530

    REFACTORED : Code to accept valid username and password

--- src/main/java/com/FirstServlet/LoginServlet.java
+++ src/main/java/com/FirstServlet/LoginServlet.java
@@ -18,17 +18,18 @@ import java.io.PrintWriter;
         }
 )
 public class LoginServlet extends HttpServlet {
+    UserValidator userValidator = new UserValidator();
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("pwd");
-        if (username.equals(getServletConfig().getInitParameter("user")) && password.equals(getServletConfig().getInitParameter("password"))) {
+        if (userValidator.validateUserName(username) && userValidator.validatePassword(password)) {
             request.setAttribute("user", username);
             request.setAttribute("password", password);
             request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
         } else {
             PrintWriter out = response.getWriter();
-            out.println("<font color=red>Either username or password is wrong........</font>");
+            out.println("<font color=red>Either username or password doesn't matches the rules ........</font>");
             getServletContext().getRequestDispatcher("/login.html").include(request, response);
         }
     }
